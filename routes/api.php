81c7d877a6a949c49280bf78d90f8119cbab999e
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-05-09 09:57:13 -0500

    Cambio en los endpoint del proyecto}

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DocumentController;
+use App\Http\Controllers\FaceApi\PersonController;
+use App\Http\Controllers\FaceApi\PersonGroupController;
 use App\Http\Controllers\FaceApiController;
 
 /*
@@ -23,12 +25,34 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
-// Route::post('/analyze/documents/ine/front', [DocumentController::class, 'analyzeFrontIne']);
-// Route::post('/analyze/documents/ine/back', [DocumentController::class, 'analyzeBackIne']);
-Route::post('/analyze/images/face-api/detect', [FaceApiController::class, 'detectFace']);
-Route::post('/analyze/images/face-api/verify/face2face', [FaceApiController::class, 'verifyFaceToFace']);
 
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::post('/analyze/documents/ine/front', [DocumentController::class, 'analyzeFrontIne']);
-    Route::post('/analyze/documents/ine/back', [DocumentController::class, 'analyzeBackIne']);
-});
+//* FACE API ENDPOINTS
+Route::post('/face-api/detect', [FaceApiController::class, 'detectFace']);
+Route::post('/face-api/analyze/face2face', [FaceApiController::class, 'verifyFaceToFace']);
+Route::post('/face-api/analyze/face2person', [FaceApiController::class, 'face2person']);
+
+//* FACE API TRAINING ENDPOINTS
+//* Person group
+Route::post('/face-api/persongroups/{personGroupId}', [PersonGroupController::class, 'create']);
+Route::get('/face-api/persongroups/{personGroupId}/training', [PersonGroupController::class, 'trainingStatus']);
+Route::post('/face-api/persongroups/{personGroupId}/train', [PersonGroupController::class, 'train']);
+
+//* Person
+Route::post('/face-api/persongroups/{personGroupId}/persons', [PersonController::class, 'create']);
+Route::get('/face-api/persongroups/{personGroupId}/persons', [PersonController::class, 'listAll']);
+Route::post('/face-api/persongroups/{personGroupId}/persons/{personId}/persistedFaces', [PersonController::class, 'addFace']);
+
+//* FORM RECOGNIZER ENDPOINTS
+Route::post('/analyze/documents/ine/front', [DocumentController::class, 'analyzeFrontIne']);
+Route::post('/analyze/documents/ine/back', [DocumentController::class, 'analyzeBackIne']);
+
+// Route::middleware(['auth:sanctum'])->group(function () {
+//     //* FACE API ENDPOINTS
+//     Route::post('/face-api/detect', [FaceApiController::class, 'detectFace']);
+//     Route::post('/face-api/verify/face2face', [FaceApiController::class, 'verifyFaceToFace']);
+//     Route::post('/face-api/verify/face2person', [FaceApiController::class, 'face2person']);
+
+//     //* FORM RECOGNIZER ENDPOINTS
+//     Route::post('/analyze/documents/ine/front', [DocumentController::class, 'analyzeFrontIne']);
+//     Route::post('/analyze/documents/ine/back', [DocumentController::class, 'analyzeBackIne']);
+// });
