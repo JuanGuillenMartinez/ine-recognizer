28caf9d88bf1ed4d65d7f240f227d9df5f3fc280
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-05-10 15:13:18 -0500

    Documentacion del proyecto implementada

--- routes/api.php
+++ routes/api.php
@@ -26,31 +26,31 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/auth/register', [AuthController::class, 'register']);
 
+//* FACE API ENDPOINTS
+Route::post('/faceapi/detect', [FaceApiController::class, 'detectFace']);
+Route::post('/faceapi/analyze/face2face', [FaceApiController::class, 'verifyFaceToFace']);
+Route::post('/faceapi/analyze/face2person', [FaceApiController::class, 'verifyFaceToPerson']);
+
+//* FACE API TRAINING ENDPOINTS
+//* Person group
+Route::get('/faceapi/persongroups', [PersonGroupController::class, 'list']);
+Route::post('/faceapi/persongroups/{personGroupId}', [PersonGroupController::class, 'create']);
+Route::get('/faceapi/persongroups/{personGroupId}/training', [PersonGroupController::class, 'trainingStatus']);
+Route::post('/faceapi/persongroups/{personGroupId}/train', [PersonGroupController::class, 'train']);
+Route::delete('/faceapi/persongroups/{personGroupId}', [PersonGroupController::class, 'delete']);
+
+//* Person
+Route::post('/faceapi/persongroups/{personGroupId}/persons', [PersonController::class, 'create']);
+Route::get('/faceapi/persongroups/{personGroupId}/persons', [PersonController::class, 'listAll']);
+Route::post('/faceapi/persongroups/{personGroupId}/persons/{personId}/persistedFaces', [PersonController::class, 'addFace']);
+Route::delete('/faceapi/persongroups/{personGroupId}/persons/{personId}', [PersonController::class, 'delete']);
+Route::delete('/faceapi/persongroups/{personGroupId}/persons/{personId}/persistedFaces/{persistedFaceId}', [PersonController::class, 'deleteFace']);
+
+//* FORM RECOGNIZER ENDPOINTS
+Route::post('/analyze/documents/ine/front', [DocumentController::class, 'analyzeFrontIne']);
+Route::post('/analyze/documents/ine/back', [DocumentController::class, 'analyzeBackIne']);
+
 
 Route::middleware(['auth:sanctum'])->group(function () {
 
-    //* FACE API ENDPOINTS
-    Route::post('/faceapi/detect', [FaceApiController::class, 'detectFace']);
-    Route::post('/faceapi/analyze/face2face', [FaceApiController::class, 'verifyFaceToFace']);
-    Route::post('/faceapi/analyze/face2person', [FaceApiController::class, 'verifyFaceToPerson']);
-    
-    //* FACE API TRAINING ENDPOINTS
-    //* Person group
-    Route::get('/faceapi/persongroups', [PersonGroupController::class, 'list']);
-    Route::post('/faceapi/persongroups/{personGroupId}', [PersonGroupController::class, 'create']);
-    Route::get('/faceapi/persongroups/{personGroupId}/training', [PersonGroupController::class, 'trainingStatus']);
-    Route::post('/faceapi/persongroups/{personGroupId}/train', [PersonGroupController::class, 'train']);
-    Route::delete('/faceapi/persongroups/{personGroupId}', [PersonGroupController::class, 'delete']);
-    
-    //* Person
-    Route::post('/faceapi/persongroups/{personGroupId}/persons', [PersonController::class, 'create']);
-    Route::get('/faceapi/persongroups/{personGroupId}/persons', [PersonController::class, 'listAll']);
-    Route::post('/faceapi/persongroups/{personGroupId}/persons/{personId}/persistedFaces', [PersonController::class, 'addFace']);
-    Route::delete('/faceapi/persongroups/{personGroupId}/persons/{personId}', [PersonController::class, 'delete']);
-    Route::delete('/faceapi/persongroups/{personGroupId}/persons/{personId}/persistedFaces/{persistedFaceId}', [PersonController::class, 'deleteFace']);
-    
-    //* FORM RECOGNIZER ENDPOINTS
-    Route::post('/analyze/documents/ine/front', [DocumentController::class, 'analyzeFrontIne']);
-    Route::post('/analyze/documents/ine/back', [DocumentController::class, 'analyzeBackIne']);
-    
 });
