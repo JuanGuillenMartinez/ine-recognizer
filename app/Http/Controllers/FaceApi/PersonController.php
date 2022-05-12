bc491a91742537f95e48dc1b400aaf5328a678b8
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-05-12 15:56:48 -0500

    Cambios en el nombre de la clase a PersonGroupPerson

--- app/Http/Controllers/FaceApi/PersonController.php
+++ app/Http/Controllers/FaceApi/PersonController.php
@@ -5,14 +5,14 @@ namespace App\Http\Controllers\FaceApi;
 use Illuminate\Http\Request;
 use App\Helpers\JsonResponse;
 use App\Http\Controllers\Controller;
-use App\Models\FaceApi\FaceApiPerson;
+use App\Models\FaceApi\PersonGroupPerson;
 
 class PersonController extends Controller
 {
     public function create(Request $request, $personGroupId)
     {
         $userData = $request->input('user_data', '');
-        $person = new FaceApiPerson($personGroupId);
+        $person = new PersonGroupPerson($personGroupId);
         $persistedPerson = $person->save($request->name, $userData);
         $personResponse = $person->get($persistedPerson->personId);
         return JsonResponse::sendResponse($personResponse);
@@ -22,7 +22,7 @@ class PersonController extends Controller
     {
         $start = $request->query('start', '');
         $top = $request->query('top', 500);
-        $persons = new FaceApiPerson($personGroupId);
+        $persons = new PersonGroupPerson($personGroupId);
         $response = $persons->list($start, $top);
         return JsonResponse::sendResponse($response);
     }
@@ -31,21 +31,21 @@ class PersonController extends Controller
     {
         $imageUrl = $request->url_image;
         $userData = $request->input('user_data', '');
-        $person = new FaceApiPerson($personGroupId);
+        $person = new PersonGroupPerson($personGroupId);
         $response = $person->addFace($personId, $imageUrl, $userData);
         return JsonResponse::sendResponse($response);
     }
 
     public function delete($personGroupId, $personId)
     {
-        $person = new FaceApiPerson($personGroupId);
+        $person = new PersonGroupPerson($personGroupId);
         $response = $person->delete($personId);
         return JsonResponse::sendResponse($response);
     }
 
     public function deleteFace($personGroupId, $personId, $persistedFaceId)
     {
-        $person = new FaceApiPerson($personGroupId);
+        $person = new PersonGroupPerson($personGroupId);
         $response = $person->deleteFace($personId, $persistedFaceId);
         return JsonResponse::sendResponse($response);
     }
---
+++ app/Models/FaceApi/PersonGroupPerson.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Models\FaceApi;
+
+use Illuminate\Support\Facades\Http;
+
+class PersonGroupPerson
+{
+    protected $baseUrl;
+    protected $detectionModel;
+    protected $headers;
+    protected $personGroupId;
+
+    public function __construct($personGroupId)
+    {
+        $this->baseUrl = env('URL_BASE_FACEAPI') . "/persongroups";
+        $this->detectionModel = 'detection_03';
+        $this->personGroupId = $personGroupId;
+        $this->headers = [
+            'Content-Type' => 'application/json',
+            'Ocp-Apim-Subscription-Key' => env('FACEAPI_SUBSCRIPTION_KEY'),
+        ];
+    }
+
+    public function save($name, $userData = '')
+    {
+        $endpoint = "{$this->baseUrl}/{$this->personGroupId}/persons";
+        $response = Http::withHeaders($this->headers)->post($endpoint, [
+            'name' => $name,
+            'userData' => $userData,
+        ]);
+        return json_decode($response);
+    }
+
+    public function addFace($personId, $imageUrl, $userData = '')
+    {
+        $endpoint = "{$this->baseUrl}/{$this->personGroupId}/persons/{$personId}/persistedFaces?userData={$userData}&detectionModel={$this->detectionModel}";
+        $response = Http::withHeaders($this->headers)->post($endpoint, [
+            'url' => $imageUrl,
+        ]);
+        return json_decode($response);
+    }
+
+    public function list($start = '', $top = 1000)
+    {
+        $endpoint = "{$this->baseUrl}/{$this->personGroupId}/persons?top={$top}";
+        if (strcmp($start, '') !== 0) {
+            $endpoint = $endpoint . "&start={$start}";
+        }
+        $response = Http::withHeaders($this->headers)->get($endpoint);
+        return json_decode($response);
+    }
+
+    public function get($personId)
+    {
+        $endpoint = "{$this->baseUrl}/{$this->personGroupId}/persons/{$personId}";
+        $response = Http::withHeaders($this->headers)->get($endpoint);
+        return json_decode($response);
+    }
+
+    public function delete($personId)
+    {
+        $endpoint = "{$this->baseUrl}/{$this->personGroupId}/persons/{$personId}";
+        $response = Http::withHeaders($this->headers)->delete($endpoint);
+        return json_decode($response);
+    }
+
+    public function deleteFace($personId, $persistedFaceId) {
+        $endpoint = "{$this->baseUrl}/{$this->personGroupId}/persons/{$personId}/persistedFaces/{$persistedFaceId}";
+        $response = Http::withHeaders($this->headers)->delete($endpoint);
+        return json_decode($response);
+    }
+}
