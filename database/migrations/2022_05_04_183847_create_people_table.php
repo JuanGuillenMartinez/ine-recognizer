043c1fee2cd18ffebbb27ed25720877adcd3f800
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-05-16 09:34:26 -0500

    Cambios en la migración de People

--- database/migrations/2022_05_04_183847_create_people_table.php
+++ database/migrations/2022_05_04_183847_create_people_table.php
@@ -15,12 +15,14 @@ return new class extends Migration
     {
         Schema::create('people', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->string('father_lastname');
             $table->string('mother_lastname');
             $table->string('clave_elector');
             $table->string('curp');
             $table->string('gender');
             $table->string('birthdate');
+            $table->string('address')->nullable();
             $table->string('ine_url');
             $table->timestamps();
         });
