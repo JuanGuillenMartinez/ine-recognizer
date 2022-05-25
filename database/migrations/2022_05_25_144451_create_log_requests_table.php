6de3929472c428e6c40d0a89b741e76e22813aa3
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-05-25 10:54:00 -0500

    Modelo y migracion de LogRequest agregadas

---
+++ database/migrations/2022_05_25_144451_create_log_requests_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('log_requests', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('token_used');
+            $table->string('ip_address');
+            $table->string('url_requested');
+            $table->string('headers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('log_requests');
+    }
+};
