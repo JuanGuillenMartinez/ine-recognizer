b3c9526e36584e259f2d3869e9296a61fe02410e
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-05-27 09:46:09 -0500

    Solo los administradores pueden iniciar sesion en el dashboard

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
@@ -36,5 +38,20 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    protected function attemptLogin(Request $request)
+    {
+        $success = $this->guard()->attempt(
+            $this->credentials($request), $request->boolean('remember')
+        );
+        if($success) {
+            if(Auth::user()->hasRole('super-admin')) {
+                return redirect()->intended('/');
+            }
+            $this->logout($request);
+            return redirect('/login');
+        }
+        return $success;
     }
 }
