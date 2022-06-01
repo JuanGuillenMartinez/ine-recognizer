1a3461d42a91f417ce6d79a6392788912c04c8f6
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-06-01 08:17:03 -0500

    Cambios en el algoritmo de clonado de objectos reactivos

--- resources/vue/helpers/Object.js
+++ resources/vue/helpers/Object.js
@@ -2,13 +2,10 @@ import { reactive } from "vue";
 
 export const clone = (object) => {
     if (object) {
-        // return reactive(structuredClone(object));
         const copy = JSON.parse(JSON.stringify(object));
         return reactive(copy);
     }
     return {};
-    console.log(object);
-    return object;
 };
 
 export const isEmpty = (object) => {
