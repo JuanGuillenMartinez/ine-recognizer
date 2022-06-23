6a0e8772b49b2ca7f1d9842cc6e1ed51273891fb
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-06-23 15:14:05 -0500

    Vue datatable implementado

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,8 @@ import App from "../vue/App.vue";
 import VueTableLite from "vue3-table-lite";
 import { createPinia } from "pinia";
 import Toast from "vue-toastification";
+import Vue3EasyDataTable from 'vue3-easy-data-table';
+import 'vue3-easy-data-table/dist/style.css';
 
 // toast-styles
 import "vue-toastification/dist/index.css";
@@ -13,4 +15,5 @@ createApp(App)
     .use(createPinia())
     .use(Toast)
     .component("table-lite", VueTableLite)
+    .component("data-table", Vue3EasyDataTable)
     .mount("#main");
