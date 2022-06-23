ccbe782d16f0e84cf247697314dabaa247fac803
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-06-23 15:48:51 -0500

    Paginacion de logs

--- app/Http/Controllers/LogRequestController.php
+++ app/Http/Controllers/LogRequestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\JsonResponse;
+use App\Http\Resources\Log\LogRequestCollectionResource;
 use App\Http\Resources\Log\LogRequestResource;
 use App\Models\LogRequest;
 use Illuminate\Http\Request;
@@ -11,7 +12,11 @@ class LogRequestController extends Controller
 {
     public function index()
     {
-        $logs = LogRequest::all();
+        $logs = LogRequest::paginate(10);
+        var_dump($logs->toArray());
+        die;
+        // return JsonResponse::sendResponse(new LogRequestCollectionResource($logs));
         return JsonResponse::sendResponse(LogRequestResource::collection($logs));
+        // return JsonResponse::sendResponse($logs);
     }
 }
